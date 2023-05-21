8126fe88b3026eff696bcec0065c661eec2c2d25
TaurusXin <taurusxin@outlook.com>
2023-05-21 22:18:50 +0100

    feat: check route for CMIN2 AS58807

--- asn.go
+++ asn.go
@@ -15,8 +15,8 @@ type Result struct {
 var (
 	rIp   = []string{"219.141.136.12", "202.106.50.1", "221.179.155.161", "202.96.209.133", "210.22.97.1", "211.136.112.200", "58.60.188.222", "210.21.196.6", "120.196.165.24", "61.139.2.69", "119.6.6.6", "211.137.96.205"}
 	rName = []string{"北京电信", "北京联通", "北京移动", "上海电信", "上海联通", "上海移动", "广州电信", "广州联通", "广州移动", "成都电信", "成都联通", "成都移动"}
-	ca    = []color.Attribute{color.FgHiYellow, color.FgHiMagenta, color.FgHiBlue, color.FgHiGreen, color.FgHiCyan, color.FgHiRed}
-	m     = map[string]string{"AS4134": "电信163 [普通线路]", "AS4809": "电信CN2 [优质线路]", "AS4837": "联通4837[普通线路]", "AS9929": "联通9929[优质线路]", "AS9808": "移动CMI [普通线路]", "AS58453": "移动CMI [普通线路]"}
+	ca    = []color.Attribute{color.FgHiYellow, color.FgHiMagenta, color.FgHiBlue, color.FgHiGreen, color.FgHiCyan, color.FgHiRed, color.FgHiYellow}
+	m     = map[string]string{"AS4134": "电信163 [普通线路]", "AS4809": "电信CN2 [优质线路]", "AS4837": "联通4837 [普通线路]", "AS9929": "联通9929 [优质线路]", "AS9808": "移动CMI [普通线路]", "AS58453": "移动CMI [普通线路]", "AS58807": "移动CMIN2 [精品线路]"}
 )
 
 func trace(ch chan Result, i int) {
@@ -49,7 +49,10 @@ func trace(ch chan Result, i int) {
 }
 
 func ipAsn(ip string) string {
-
+	cmin2 := []string{"223.119.8", "223.119.32", "223.119.34", "223.119.35", "223.119.36", "223.119.37",
+		"223.119.100", "223.120.128", "223.120.134", "223.120.138", "223.120.158", "223.120.164", "223.120.165",
+		"223.120.168", "223.120.172", "223.120.174", "223.120.184", "223.120.188", "223.120.192", "223.120.200",
+		"223.120.210", "223.120.212"}
 	switch {
 	case strings.HasPrefix(ip, "59.43"):
 		return "AS4809"
@@ -60,6 +63,11 @@ func ipAsn(ip string) string {
 	case strings.HasPrefix(ip, "219.158"):
 		return "AS4837"
 	case strings.HasPrefix(ip, "223.118") || strings.HasPrefix(ip, "223.119") || strings.HasPrefix(ip, "223.120") || strings.HasPrefix(ip, "223.121"):
+		for _, prefix := range cmin2 {
+			if strings.HasPrefix(ip, prefix) {
+				return "AS58807"
+			}
+		}
 		return "AS58453"
 	default:
 		return ""
