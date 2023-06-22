b1ee58ca93f055a882d8b90f60b1a9d6ebe29f68
taurusxin <taurusxin@outlook.com>
2023-06-22 12:20:58 +0100

    update: install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 arch=$(uname -m)
 if [ "$arch" = "x86_64" ]; then
-  wget -q -O backtrace.tar.gz  https://github.com/zhanghanyun/backtrace/releases/latest/download/backtrace-linux-amd64.tar.gz
+  wget -q -O backtrace.tar.gz  https://github.com/taurusxin/backtrace/releases/latest/download/backtrace-linux-amd64.tar.gz
 else
-  wget -q -O backtrace.tar.gz  https://github.com/zhanghanyun/backtrace/releases/latest/download/backtrace-linux-arm64.tar.gz
+  wget -q -O backtrace.tar.gz  https://github.com/taurusxin/backtrace/releases/latest/download/backtrace-linux-arm64.tar.gz
 fi
 
 tar -xf backtrace.tar.gz
 rm backtrace.tar.gz
-mv backtrace /usr/bin/
+mv backtrace /usr/local/bin/
 backtrace
